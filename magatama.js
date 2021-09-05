6a1ad9d3a704ca807b14eef67ea1787a272a86f6
terithian <90120453+terithian@users.noreply.github.com>
2021-09-04 22:38:08 -0700

    Finished Filling in Info
    All the info is now filled in. It is now done.

--- magatama.js
+++ magatama.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
     var current_open = 0;
     $('.magatama-entry').on('click', function() {
-        var entrynum = $(this).attr('data-entry');
+        var this_entry = $(this);
+        var entrynum = this_entry.attr('data-entry');
         if ( entrynum == current_open ) {
             current_open = 0;
+            this_entry.removeClass('active');
             $('.magatama-info.entry' + entrynum).removeClass('active').stop().slideUp(300);
         }
         else {
             current_open = entrynum;
             
             if ( $('.magatama-info.active').length ) {
+                $('.magatama-entry').removeClass('active');
                 $('.magatama-info.active').removeClass('active').stop().slideUp(300, function() {
+                    this_entry.addClass('active');
                     $('.magatama-info.entry' + entrynum).addClass('active').stop().slideDown(500);
                 });
             }
             else {
+                this_entry.addClass('active');
                 $('.magatama-info.entry' + entrynum).addClass('active').stop().slideDown(500);
             }
         }
